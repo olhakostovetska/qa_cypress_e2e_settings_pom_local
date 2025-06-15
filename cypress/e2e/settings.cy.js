e118098a7d094e1a6f1194dc89bc7f51a61a63e8
olhakostovetska <kostovetskaolha@gmail.com>
2025-06-15 17:20:22 +0300

    solution

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,71 @@
+/* eslint-disable max-len */
+/* eslint-disable cypress/unsafe-to-chain-command */
+// eslint-disable cypress/unsafe-to-chain-command
+// eslint-disable max-len
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
 describe('Settings page', () => {
-  before(() => {
-
-  });
+  let email;
 
   beforeEach(() => {
+    const timestamp = Date.now();
+    email = `yurii${timestamp}@gmail.com`;
 
+    cy.request('POST', 'http://localhost:3000/api/users', {
+      user: {
+        username: `yurii${timestamp}`,
+        email,
+        password: '1234567Qwerty',
+        bio: 'Test user bio',
+      }
+    });
+
+    cy.visit('/user/login');
+    cy.get('input[placeholder="Email"]').type(email);
+    cy.get('input[placeholder="Password"]').type('1234567Qwerty');
+    cy.contains('button', 'Sign in').click();
+    cy.location('pathname', { timeout: 10000 }).should('not.include', '/login');
+    cy.visit('/settings');
   });
 
   it('should provide an ability to update username', () => {
-
+    cy.get('input[placeholder="Username"]').clear().type('superQA_user');
+    cy.get('input[placeholder="Email"]').clear().type(email);
+    cy.get('input[placeholder="New Password"]').clear().type('1234567Qwerty');
+    cy.contains('button', 'Update Settings').click();
   });
 
   it('should provide an ability to update bio', () => {
-
+    cy.get('textarea[placeholder="Short bio about you"]').clear().type('I am the king of Cypress!');
+    cy.get('input[placeholder="Username"]').clear().type('superQA_user');
+    cy.get('input[placeholder="Email"]').clear().type(email);
+    cy.get('input[placeholder="New Password"]').clear().type('1234567Qwerty');
+    cy.contains('button', 'Update Settings').click();
   });
 
   it('should provide an ability to update an email', () => {
-
+    cy.get('input[placeholder="Email"]').clear().type(`superqa_test_${Date.now()}@gmail.com`);
+    cy.get('input[placeholder="Username"]').clear().type('superQA_user');
+    cy.get('input[placeholder="New Password"]').clear().type('1234567Qwerty');
+    cy.contains('button', 'Update Settings').click();
   });
 
   it('should provide an ability to update password', () => {
-
+    cy.get('input[placeholder="New Password"]').type('MegaSecret123!');
+    cy.get('input[placeholder="Username"]').clear().type('superQA_user');
+    cy.get('input[placeholder="Email"]').clear().type(email);
+    cy.contains('button', 'Update Settings').click();
   });
 
-  it('should provide an ability to log out', () => {
+it('should provide an ability to log out', () => {
+  cy.contains('button', 'Or click here to logout.').click();
 
-  });
+  cy.url({ timeout: 10000 }).should('eq', 'http://localhost:3000/');
+
+  // Перевірка наявності кнопки Sign in після logout
+  cy.contains('a', 'Sign in').should('be.visible');
+});
+
+
 });
--- test_lib.js
+++ test_lib.js
@@ -1,28 +1,29 @@
+/* eslint-disable no-console */
 /// Need a separate file from test.js because Mocha automatically defines stuff like it,
 // which would break non-Mocha requirers.
 
-const perf_hooks = require('perf_hooks')
+const perf_hooks = require('perf_hooks');
 
-const models = require('./models')
+const models = require('./models');
 
-const now = perf_hooks.performance.now
+const now = perf_hooks.performance.now;
 
 // https://stackoverflow.com/questions/563406/add-days-to-javascript-date
 function addDays(oldDate, days) {
-  const newDate = new Date(oldDate.valueOf())
-  newDate.setDate(oldDate.getDate() + days)
-  return newDate
+  const newDate = new Date(oldDate.valueOf());
+  newDate.setDate(oldDate.getDate() + days);
+  return newDate;
 }
-const DATE0 = new Date(2000, 0, 0, 0, 0, 0, 0)
+const DATE0 = new Date(2000, 0, 0, 0, 0, 0, 0);
 
 function makeComment(articleId, authorId, i) {
   return {
     body: `my comment ${i}`,
     articleId,
     authorId,
-  }
+  };
 }
-exports.makeComment = makeComment
+exports.makeComment = makeComment;
 
 function makeArticle(i = 0, opts) {
   let ret = {
@@ -64,167 +65,167 @@ List:
 - item 2
 - item 3
 `,
-  }
+  };
   if (!opts.api) {
-    const date = opts.date === undefined ? DATE0 : opts.date
+    const date = opts.date === undefined ? DATE0 : opts.date;
     Object.assign(ret, {
       authorId: opts.authorId,
       createdAt: date,
       updatedAt: date,
-    })
+    });
   }
-  return ret
+  return ret;
 }
-exports.makeArticle = makeArticle
+exports.makeArticle = makeArticle;
 
 function makeTag(i) {
-  return { name: `tag${i}` }
+  return { name: `tag${i}` };
 }
-exports.makeTag = makeTag
+exports.makeTag = makeTag;
 
 function makeUser(sequelize, i = 0) {
   const userArg = {
     username: `user${i}`,
     email: `user${i}@mail.com`,
-  }
+  };
   if (i % 2 === 0) {
-    userArg.bio = `My bio ${i}`
+    userArg.bio = `My bio ${i}`;
   }
-  const password = 'asdf'
+  const password = 'asdf';
   if (sequelize) {
-    sequelize.models.User.setPassword(userArg, password)
+    sequelize.models.User.setPassword(userArg, password);
   } else {
-    userArg.password = password
+    userArg.password = password;
   }
-  return userArg
+  return userArg;
 }
-exports.makeUser = makeUser
+exports.makeUser = makeUser;
 
-let printTimeNow
+let printTimeNow;
 function printTime() {
-  const newNow = now()
-  console.error((newNow - printTimeNow) / 1000.0)
-  printTimeNow = newNow
+  const newNow = now();
+  console.error((newNow - printTimeNow) / 1000.0);
+  printTimeNow = newNow;
 }
 
 async function generateDemoData(params) {
-  const nUsers = params.nUsers === undefined ? 10 : params.nUsers
+  const nUsers = params.nUsers === undefined ? 10 : params.nUsers;
   const nArticlesPerUser =
-    params.nArticlesPerUser === undefined ? 10 : params.nArticlesPerUser
+    params.nArticlesPerUser === undefined ? 10 : params.nArticlesPerUser;
   const nMaxCommentsPerArticle =
     params.nMaxCommentsPerArticle === undefined
       ? 3
-      : params.nMaxCommentsPerArticle
+      : params.nMaxCommentsPerArticle;
   const nMaxTagsPerArticle =
-    params.nMaxTagsPerArticle === undefined ? 3 : params.nMaxTagsPerArticle
+    params.nMaxTagsPerArticle === undefined ? 3 : params.nMaxTagsPerArticle;
   const nFollowsPerUser =
-    params.nFollowsPerUser === undefined ? 2 : params.nFollowsPerUser
+    params.nFollowsPerUser === undefined ? 2 : params.nFollowsPerUser;
   const nFavoritesPerUser =
-    params.nFavoritesPerUser === undefined ? 5 : params.nFavoritesPerUser
-  const nTags = params.nTags === undefined ? 10 : params.nTags
-  const directory = params.directory
-  const basename = params.basename
-  const verbose = params.verbose === undefined ? false : params.verbose
+    params.nFavoritesPerUser === undefined ? 5 : params.nFavoritesPerUser;
+  const nTags = params.nTags === undefined ? 10 : params.nTags;
+  const directory = params.directory;
+  const basename = params.basename;
+  const verbose = params.verbose === undefined ? false : params.verbose;
 
-  const nArticles = nUsers * nArticlesPerUser
+  const nArticles = nUsers * nArticlesPerUser;
 
-  let sequelize
+  let sequelize;
   if (params.sequelize) {
-    sequelize = params.sequelize
+    sequelize = params.sequelize;
   } else {
-    sequelize = models.getSequelize(directory, basename)
+    sequelize = models.getSequelize(directory, basename);
   }
-  await models.sync(sequelize, { force: true })
+  await models.sync(sequelize, { force: true });
   if (!params.empty) {
-    printTimeNow = now()
-    if (verbose) console.error('User')
-    const userArgs = []
+    printTimeNow = now();
+    if (verbose) console.error('User');
+    const userArgs = [];
     for (let i = 0; i < nUsers; i++) {
-      userArgs.push(makeUser(sequelize, i))
+      userArgs.push(makeUser(sequelize, i));
     }
-    const users = await sequelize.models.User.bulkCreate(userArgs)
-    if (verbose) printTime()
+    const users = await sequelize.models.User.bulkCreate(userArgs);
+    if (verbose) printTime();
 
-    if (verbose) console.error('UserFollowUser')
-    const followArgs = []
+    if (verbose) console.error('UserFollowUser');
+    const followArgs = [];
     for (let i = 0; i < nUsers; i++) {
-      const userId = users[i].id
+      const userId = users[i].id;
       for (let j = 0; j < nFollowsPerUser; j++) {
         followArgs.push({
           userId: userId,
           followId: users[(i + 1 + j) % nUsers].id,
-        })
+        });
       }
     }
-    await sequelize.models.UserFollowUser.bulkCreate(followArgs)
-    if (verbose) printTime()
+    await sequelize.models.UserFollowUser.bulkCreate(followArgs);
+    if (verbose) printTime();
 
-    if (verbose) console.error('Article')
-    const articleArgs = []
+    if (verbose) console.error('Article');
+    const articleArgs = [];
     for (let i = 0; i < nArticles; i++) {
-      const userIdx = i % nUsers
-      const date = addDays(DATE0, i)
-      articleArgs.push(makeArticle(i, { authorId: users[userIdx].id, date }))
+      const userIdx = i % nUsers;
+      const date = addDays(DATE0, i);
+      articleArgs.push(makeArticle(i, { authorId: users[userIdx].id, date }));
     }
     const articles = await sequelize.models.Article.bulkCreate(articleArgs, {
       validate: true,
       individualHooks: true,
-    })
-    if (verbose) printTime()
+    });
+    if (verbose) printTime();
 
-    if (verbose) console.error('UserFavoriteArticle')
-    let articleIdx = 0
-    const favoriteArgs = []
+    if (verbose) console.error('UserFavoriteArticle');
+    let articleIdx = 0;
+    const favoriteArgs = [];
     for (let i = 0; i < nUsers; i++) {
-      const userId = users[i].id
+      const userId = users[i].id;
       for (let j = 0; j < nFavoritesPerUser; j++) {
         favoriteArgs.push({
           userId: userId,
           articleId: articles[articleIdx % nArticles].id,
-        })
-        articleIdx += 1
+        });
+        articleIdx += 1;
       }
     }
-    await sequelize.models.UserFavoriteArticle.bulkCreate(favoriteArgs)
-    if (verbose) printTime()
+    await sequelize.models.UserFavoriteArticle.bulkCreate(favoriteArgs);
+    if (verbose) printTime();
 
-    if (verbose) console.error('Tag')
-    const tagArgs = []
+    if (verbose) console.error('Tag');
+    const tagArgs = [];
     for (let i = 0; i < nTags; i++) {
-      tagArgs.push(makeTag(i))
+      tagArgs.push(makeTag(i));
     }
-    const tags = await sequelize.models.Tag.bulkCreate(tagArgs)
-    if (verbose) printTime()
+    const tags = await sequelize.models.Tag.bulkCreate(tagArgs);
+    if (verbose) printTime();
 
-    if (verbose) console.error('ArticleTag')
-    let tagIdx = 0
-    const articleTagArgs = []
+    if (verbose) console.error('ArticleTag');
+    let tagIdx = 0;
+    const articleTagArgs = [];
     for (let i = 0; i < nArticles; i++) {
       for (let j = 0; j < i % (nMaxTagsPerArticle + 1); j++) {
         articleTagArgs.push({
           articleId: articles[i].id,
           tagId: tags[tagIdx % nTags].id,
-        })
-        tagIdx += 1
+        });
+        tagIdx += 1;
       }
     }
-    await sequelize.models.ArticleTag.bulkCreate(articleTagArgs)
-    if (verbose) printTime()
+    await sequelize.models.ArticleTag.bulkCreate(articleTagArgs);
+    if (verbose) printTime();
 
-    if (verbose) console.error('Comment')
-    const commentArgs = []
-    let commentIdx = 0
+    if (verbose) console.error('Comment');
+    const commentArgs = [];
+    let commentIdx = 0;
     for (let i = 0; i < nArticles; i++) {
       for (let j = 0; j < i % (nMaxCommentsPerArticle + 1); j++) {
         commentArgs.push(
           makeComment(articles[i].id, users[commentIdx % nUsers].id, commentIdx)
-        )
+        );
       }
     }
-    await sequelize.models.Comment.bulkCreate(commentArgs)
-    if (verbose) printTime()
+    await sequelize.models.Comment.bulkCreate(commentArgs);
+    if (verbose) printTime();
   }
 
-  return sequelize
+  return sequelize;
 }
-exports.generateDemoData = generateDemoData
+exports.generateDemoData = generateDemoData;
